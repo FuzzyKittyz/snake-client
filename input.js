5d3f311367edea5f17071a9a4d1f850c73770e25
FuzzyKittyz <topher.a@hotmail.com>
2022-09-05 13:42:16 -0600

    make snake boy go woosh and move

--- input.js
+++ input.js
@@ -1,17 +1,34 @@
 const net = require("net");
 
-const setupInput = function () {
+let connection;
+
+const setupInput = function (conn) {
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
+  connection = conn;
+ 
+  const handleUserInput = function (key) { //handles all inputs made by the user 
+  if (key === '\u0003') {// for quiting the app
+    process.exit();
+  }
+  if (key === 'w') {//next couple all deal with WASD movement
+    connection.write('Move: up')
+  }
+  if (key === 'a') {
+    connection.write("Move: left")
+  }
+  if (key === 's') {
+    connection.write("Move: down")
+  }
+  if (key === 'd') {
+    connection.write("Move: right")
+  }
 };
 
-const handleUserInput = function (key) {
-  if (key === '\u0003') {
-    process.exit();
-  }}
+stdin.on("data", handleUserInput);
+return stdin
+};
 
   module.exports = setupInput;
